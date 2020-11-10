999d65c0f051f6c1632d302ae9d891f31b86ae81
menahishayan <menahi.shayan@gmail.com>
2020-11-10 15:23:41 +0530

    button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div>
       <h1>Better Banking</h1>
+      <Button type="primary">Halo</Button>
     </div>
   );
 }
