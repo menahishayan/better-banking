01ade2e0381b8e037f4eba44e49172770390a09c
menahishayan <menahi.shayan@gmail.com>
2020-11-11 13:54:57 +0530

    Removed login CSS interfering with Dashboard

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 import './Login.css'
+
 function Login() {
     return (
         <div>
-          
             <h1>Login</h1>
-            <from action="signin.html" method="post">
+            <form action="signin.html" method="post">
                 <div className="username">
                      <input id="username" type="Username" required placeholder="Username"></input>
                  </div>
@@ -14,8 +13,7 @@ function Login() {
                      <input id="password" type="Password" required placeholder="Password"></input>
                  </div>
                  <button>Login</button>
-            </from>
-            
+            </form>
         </div>
     );
 }
