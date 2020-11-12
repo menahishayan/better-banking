57e2847acb809cc73947e2a1d9f2bf64305766ed
ruby <rubyseher2002@gmail.com>
2020-11-12 17:34:42 +0530

    profile bg

--- src/Profile.js
+++ src/Profile.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import './Profile.css';
 
 function Profile() {
     return (
-        <div>
-            <h1>Profile</h1>
+        <div className='profile-back'>
+            {/* <h1>Profile</h1>
             <div className="person zoom-m">
-                        <div className="person-pic"></div>
-                        <span className="person-name">Nishank</span>
-                    </div>
-            <Button type="primary">
-                Back
-            </Button>
+                <div className="person-pic"></div>
+                <span className="person-name">Nishank</span>
+            </div>
+            <Button type="primary">Back</Button> */}
+            <div className="person zoom-m">
+                <div className="person-pic"></div>
+                <span className="person-name">Nishank</span>
+            </div>
         </div>
     );
 }
