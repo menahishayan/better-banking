8a932a384ffc0e63bdf5ef6782e224664fceacd5
menahishayan <menahi.shayan@gmail.com>
2020-11-12 14:22:16 +0530

    login page

--- src/Login.js
+++ src/Login.js
@@ -13,9 +13,16 @@ function Login() {
     const [user, setUser] = useState();
     const { register, handleSubmit } = useForm()
 
-    const loginHandler = () => {
+    const loginHandler = (d) => {
         if (!user) {
-            db.getUser('0192319023103', (user) => {
+            let id = ''
+            switch(d.email) {
+                case 'menahi.shayan@gmail.com': id='P1UUbhJEx6T63ceqWw1RDGKXmo12'; break;
+                case 'nishank.swamy@gmail.com': id='Gkv2Z8gyfINuWc5ofF8C1BVGwAm2'; break;
+                case 'revathipodaralla123@gmail.com': id='GRxen20Hy2PWt00dxEj4LRk9z5V2'; break;
+                default: console.log('login error');
+            }
+            db.getUser(id, (user) => {
                 setUser(user);
                 setRedirect('/dashboard')
             })
@@ -25,11 +32,14 @@ function Login() {
     if (redirect) return <Redirect push to={{ pathname: redirect, state: user }} />
     return (
         <div className="login-bg">
+            <h1 className="title">Better Banking</h1>
             <Form className='login-container' onSubmit={handleSubmit(loginHandler)}>
                 <h2>Login</h2>
-                <Form.Control type="text" name='username' placeholder='Username' className="textfield field" ref={register({ required: true })} /><br />
-                <Form.Control type="password" name='password' placeholder='Password' className="textfield field" ref={register({ required: true })} /><br />
+                <br/>
+                <Form.Control type="email" name='email' placeholder='Email' className="textfield field" ref={register({ required: true })} />
+                <Form.Control type="password" name='password' placeholder='Password' className="textfield field" ref={register({ required: true })} />
                 <Button type="submit" className='submit'>Submit</Button>
+                <br/><br/><br/>
             </Form>
         </div>
     );
