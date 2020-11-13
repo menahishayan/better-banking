908ecda073a26cd5a9be88111c503338b19c2f41
ruby <rubyseher2002@gmail.com>
2020-11-13 22:22:09 +0530

    login info

--- src/Profile.js
+++ src/Profile.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft} from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import './Profile.css';
 
 function Profile(props) {
@@ -13,19 +13,20 @@ function Profile(props) {
     console.log(user);
     return (
         <div>
-            <FontAwesomeIcon icon={faArrowLeft} className="back-button zoom-m" onClick={() => setRedirect('/dashboard')}/>
+            <FontAwesomeIcon icon={faArrowLeft} className="back-button zoom-m" onClick={() => setRedirect('/dashboard')} />
             <div className='profile-back'> &nbsp;</div>
             <div className='profile-subcontent'>
                 <div className="profile zoom-m">
                     <div className="profile-pic"></div>
                     <span className="person-name">{user.name}</span>
                 </div><br />
-                <div className='info'><b>Short Name :</b> {user.name}</div>
-                <div className='info'><b>Account Number:</b> {user.name}</div>
-                <div className='info'><b>Date of Birth :</b> {user.name}</div>
-                <div className='info'><b>Phone :</b> {user.name}</div>
-                <div className='info'><b>Email :</b> {user.name}</div>
-                <div className='info'><b>UPI ID :</b> {user.name}</div>
+                <div className='edit'><FontAwesomeIcon icon={faArrowLeft}/></div>
+                <div className='info'><b>Short Name :</b> {user.shortname}</div>
+                <div className='info'><b>Account Number:</b> {user.accno}</div>
+                <div className='info'><b>Date of Birth :</b> {user.dob}</div>
+                <div className='info'><b>Phone :</b> {user.phone}</div>
+                <div className='info'><b>Email :</b> {user.email}</div>
+                <div className='info'><b>UPI ID :</b> {user.upi}</div>
                 <div className='info'><b>change Password :</b> {user.name}</div>
                 {/* <div>
                     {   
