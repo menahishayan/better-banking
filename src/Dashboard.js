52c5c376e61476fd19f4432afb72cfba66eec2d9
Shayan <shayan1232001@yahoo.co.in>
2020-11-14 20:45:54 +0530

    components

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,5 +1,5 @@
 import './Dashboard.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { useForm } from "react-hook-form";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
@@ -7,13 +7,10 @@ import Spinner from 'react-bootstrap/Spinner'
 import { Redirect } from 'react-router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faUtensils, faPlane, faShoppingBag } from '@fortawesome/free-solid-svg-icons'
-import { PieChart, Pie, Cell, Tooltip } from 'recharts';
 import DB from './DB'
-import { Overlay } from './Components'
+import { Overlay, Chart, Card, PersonAvatar, COLORS } from './Components'
 
 const db = new DB()
-
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 function Dashboard(props) {
     const [redirect, setRedirect] = useState();
@@ -27,10 +24,6 @@ function Dashboard(props) {
     const { register, handleSubmit } = useForm()
 
     var user = props.location.state
-
-    const maskedCardNo = (number) => {
-        return number.substring(0, 4) + number.substring(4, number.length - 2).replace(/\d/g, "\u2022") + number.substring(number.length - 2);
-    }
 
     useEffect(() => {
         db.getProfilePic(user.accno, (url) => {
@@ -66,31 +59,6 @@ function Dashboard(props) {
         getRecentPersons()
     }, [user.accno, user.history])
 
-    const chartLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-        const radius = innerRadius + (outerRadius - innerRadius) * 0.25;
-        const x = cx + radius * Math.cos(-midAngle * (Math.PI / 180));
-        const y = cy + radius * Math.sin(-midAngle * (Math.PI / 180));
-
-        return (
-            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-                {chartData[index].name}
-            </text>
-        );
-    };
-
-    const chartTooltip = ({ active, payload, label }) => {
-        if (active) {
-            return (
-                <div className="chart-tooltip">
-                    <b>{`${payload[0].name}`}</b><br />
-                    {` \u20B9${payload[0].value.toFixed(2)}`}
-                </div>
-            );
-        }
-
-        return null;
-    };
-
     const payHandler = (d) => {
         console.log(d);
     }
@@ -116,16 +84,7 @@ function Dashboard(props) {
                 <div className="recents">
                     {
                         recentPersons && recentPersons.map((person, p) =>
-                            <div className="person zoom-m" key={p} onClick={() => { setPayOverlay(true); setNewPayment({ person }) }}>
-                                {
-                                    person.img ?
-                                        <div>
-                                            <img src={person.img} alt={person.shortname} className="person-pic" />
-                                        </div> :
-                                        <div className="person-pic"></div>
-                                }
-                                <span className="person-name">{person.shortname}</span>
-                            </div>
+                            <Fragment key={p}><PersonAvatar person={person} onClick={() => { setPayOverlay(true); setNewPayment({ person }) }} /></Fragment>
                         )
                     }
                 </div>
@@ -133,13 +92,7 @@ function Dashboard(props) {
             <div className="dashboard-subcontent">
                 <div style={{ width: '78%' }}>
                     <h1>History</h1>
-                    <PieChart width={300} height={300} className="chart">
-                        <Pie data={chartData} labelLine={false} label={chartLabel} outerRadius={150} innerRadius={110} fill="#8884d8" dataKey="value" >
-                            {chartData.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)}
-                        </Pie>
-                        <Tooltip content={chartTooltip} />
-                        {/* <Legend /> */}
-                    </PieChart>
+                    <Chart chartData={chartData} />
                     <div className="history-list">
                         { //add txn id & to and from tracking on both sides
                             user.history.map((item, i) => (
@@ -155,36 +108,15 @@ function Dashboard(props) {
                 </div>
                 <div className="card-section">
                     <h1>Cards</h1>
-                    <div className="card zoom-l">
-                        <div className="card-type">
-                            <img src="visa-icon.svg" alt="visa" height="18px" />
-                        </div>
-                        <div className="card-no">{maskedCardNo(user.cards[0].number)}</div>
-                    </div>
+                    <Card cardNo={user.cards[0].number} />
                 </div>
 
             </div>
             { payOverlay &&
-                <Overlay visible={payOverlay} bgClick={() => setPayOverlay(!payOverlay)} height={25} width={50}>
-
+                <Overlay visible={payOverlay} bgClick={() => setPayOverlay(!payOverlay)}>
                     <Form onSubmit={handleSubmit(payHandler)}>
-                        <div style={{ display: 'inline-block' }}>
-                            <div style={{ display: 'inline-block' }}>
-                                {
-                                    newPayment.person.img ?
-                                        <div>
-                                            <img src={newPayment.person.img} alt={newPayment.person.shortname} className="person-pic" />
-                                        </div> :
-                                        <div className="person-pic"></div>
-                                }
-                            </div>
-                            <div style={{ display: 'inline-block' }}>
-
-                            <span style={{fontSize:22, fontWeight:'bold'}}>Pay {newPayment.person.name}</span><br/>
-                            <span>{newPayment.person.accno}</span>
-                        </div>
-                        </div>
-
+                            <PersonAvatar person={newPayment.person} inline />
+                        <br />
                         <br />
                         <Form.Control type="number" name='amount' placeholder='Amount' className="textfield field" ref={register({ required: true })} />
                         <Form.Control type="text" name='description' placeholder='Message' className="textfield field" ref={register({ required: false })} />
