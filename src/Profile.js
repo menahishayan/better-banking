48219b57655dac57966c0345e1a65a8171e6b061
ruby <rubyseher2002@gmail.com>
2020-11-14 09:14:55 +0530

    change bassword button

--- src/Profile.js
+++ src/Profile.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { Redirect } from 'react-router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft,faPencilAlt } from '@fortawesome/free-solid-svg-icons'
+import Button from 'react-bootstrap/Button'
 import './Profile.css';
+import DB from './DB'
+
+const db = new DB()
 
 function Profile(props) {
     const [redirect, setRedirect] = useState();
+    const [profilePic, setProfilePic] = useState();
 
     var user = props.location.state;
+    useEffect(() => {
+        db.getProfilePic(user.accno, (url) => {
+            setProfilePic(url)
+        })
+    })
     if (redirect) return <Redirect push to={{ pathname: redirect, state: user }} />
 
     console.log(user);
@@ -17,17 +27,17 @@ function Profile(props) {
             <div className='profile-back'> &nbsp;</div>
             <div className='profile-subcontent'>
                 <div className="profile zoom-m">
-                    <div className="profile-pic"></div>
+                    <div ><img src={profilePic} className="profile-pic"/></div>
                     <span className="person-name">{user.name}</span>
                 </div><br />
-                <div className='edit'><FontAwesomeIcon icon={faArrowLeft}/></div>
+                <div className='edit'><FontAwesomeIcon icon={faPencilAlt}/></div>
                 <div className='info'><b>Short Name :</b> {user.shortname}</div>
                 <div className='info'><b>Account Number:</b> {user.accno}</div>
                 <div className='info'><b>Date of Birth :</b> {user.dob}</div>
                 <div className='info'><b>Phone :</b> {user.phone}</div>
                 <div className='info'><b>Email :</b> {user.email}</div>
                 <div className='info'><b>UPI ID :</b> {user.upi}</div>
-                <div className='info'><b>change Password :</b> {user.name}</div>
+                <Button className='change'>Change Password</Button>
                 {/* <div>
                     {   
                         Object.keys(user).map((item, i) => (
